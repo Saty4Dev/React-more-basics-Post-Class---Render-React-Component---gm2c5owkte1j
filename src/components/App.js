a8627d9db5595f66a83fed9a2cf04867182ff0a6
Saty4Dev <57029286+Saty4Dev@users.noreply.github.com>
2022-02-16 01:31:23 +0530

    Initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import ParaComp from "./ParaComp";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <ParaComp />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/ParaComp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ParaComp = () => {
+return (
+  <p>I am learning React. My life is getting better.</p>
+
+)
+}
+
+
+export default ParaComp;
